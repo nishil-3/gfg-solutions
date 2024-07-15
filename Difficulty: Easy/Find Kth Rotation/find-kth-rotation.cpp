8f085f8fcf78cf1a87dfb15815d286b0a1677e71
Nishil <128979710+nishil-3@users.noreply.github.com>
2024-07-15 12:22:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -9,12 +9,21 @@ using namespace std;
 class Solution {
   public:
     int findKRotation(vector<int> &arr) {
-        for (int i=1;i<arr.size();i++)
-        {
-            if (arr[i-1]>arr[i]) return i;
-        }
         // Code Here
-        return 0;
+        
+        
+         int n=arr.size();
+        int low=0, high=n-1, ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]<arr[0]){ // As we need to find out the index of the smallest element                                               as that much place it has been pushed due to rotation
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            low=mid+1;
+        }
+        return ans;
     }
 };
 
